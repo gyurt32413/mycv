875ad244be42507b85b8a80136fa6c2f5d77aecc
leju-Steven <steven@leju.com.tw>
2026-06-23 00:36:49 +0800

    新增用戶註冊功能，包含 DTO、控制器及請求範例；更新 ESLint 配置及 package.json 依賴

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // 自動刪除DTO中不存在的屬性
+      whitelist: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

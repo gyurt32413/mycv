3ee8c7421e0e18df5ff5190e2328ed7f43a8c575
leju-Steven <steven@leju.com.tw>
2026-08-12 00:24:32 +0800

    新增 TypeORM 生命週期事件裝飾器至 User 實體，並更新 UsersService 的註解

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import {
+  AfterUpdate,
+  AfterRemove,
+  AfterInsert,
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+} from 'typeorm';
 
 // 慣例命名 User 不用 UserEntity
 @Entity()
@@ -12,4 +19,20 @@ export class User {
 
   @Column()
   password!: string;
+
+  // AfterInsert 是 TypeORM 提供的生命週期事件裝飾器(hook)，會在實體被插入資料庫後觸發
+  @AfterInsert()
+  logInsert() {
+    console.log(`User with id ${this.id} has been inserted`);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log(`User with id ${this.id} has been updated`);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log(`User with id ${this.id} has been removed`);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -13,7 +13,13 @@ export class UsersService {
   // }
 
   create(email: string, password: string) {
+    // create 方法會建立一個新的 User 實例，但不會立即存入資料庫
     const user = this.repo.create({ email, password });
+
+    // save 方法會將 User 實例存入資料庫，並返回存入後的 User 實例
     return this.repo.save(user);
+
+    // 另一種寫法，但如果用這種寫法，寫在 entity 裡的驗證就不會生效
+    // return this.repo.save({ email, password });
   }
 }

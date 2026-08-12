74f3946593cd064513589330b2798c9bb044ccca
leju-Steven <steven@leju.com.tw>
2026-08-13 00:38:37 +0800

    新增用戶查詢、更新及刪除功能，並處理找不到用戶的情況

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -22,4 +22,37 @@ export class UsersService {
     // 另一種寫法，但如果用這種寫法，寫在 entity 裡的驗證就不會生效
     // return this.repo.save({ email, password });
   }
+
+  findOne(id: number) {
+    return this.repo.findOne({ where: { id } });
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  // 使用 Partial 表示滿足 User 其中部分屬性即可，這樣就可以只傳入要更新的屬性，而不需要傳入整個 User 物件
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.repo.findOne({ where: { id } });
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+
+    // 使用 TypeORM 的 update 方法，這樣就不會觸發 entity 裡的生命週期事件(hook)，所以不建議使用
+    // return this.repo.update(id, attrs);
+  }
+
+  async remove(id: number) {
+    const user = await this.repo.findOne({ where: { id } });
+    if (!user) {
+      throw new Error('User not found');
+    }
+    return this.repo.remove(user);
+
+    // 使用 TypeORM 的 delete 方法，這樣就不會觸發 entity 裡的生命週期事件(hook)，所以不建議使用
+    // return this.repo.delete(id);
+  }
 }

9f2325acd10e5ad3fbe6de15eecc6314283217af
leju-Steven <steven@leju.com.tw>
2026-08-13 23:58:06 +0800

    更新 UsersService，將錯誤處理從 Error 轉為 NotFoundException，並在 findOne 方法中新增異步處理

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -23,8 +23,12 @@ export class UsersService {
     // return this.repo.save({ email, password });
   }
 
-  findOne(id: number) {
-    return this.repo.findOne({ where: { id } });
+  async findOne(id: number) {
+    const user = await this.repo.findOne({ where: { id } });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
   }
 
   find(email: string) {
@@ -35,7 +39,7 @@ export class UsersService {
   async update(id: number, attrs: Partial<User>) {
     const user = await this.repo.findOne({ where: { id } });
     if (!user) {
-      throw new Error('User not found');
+      throw new NotFoundException('User not found');
     }
 
     Object.assign(user, attrs);
@@ -48,7 +52,7 @@ export class UsersService {
   async remove(id: number) {
     const user = await this.repo.findOne({ where: { id } });
     if (!user) {
-      throw new Error('User not found');
+      throw new NotFoundException('User not found');
     }
     return this.repo.remove(user);
 

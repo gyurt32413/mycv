c0db8beb236636bcced23879b7e15ea5649ce6b7
leju-Steven <steven@leju.com.tw>
2026-08-13 23:22:35 +0800

    新增用戶查詢及查詢所有用戶功能，更新 UsersController 以支持相應的 GET 請求

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Query, Param } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -9,5 +9,17 @@ export class UsersController {
   @Post('/signup')
   async createUser(@Body() body: CreateUserDto) {
     await this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    return user;
+  }
+
+  @Get()
+  async findAllUsers(@Query('email') email: string) {
+    const users = await this.usersService.find(email);
+    return users;
   }
 }

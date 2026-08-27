1d21b77666e1c2ef3859d5e326f623c5661bb660
leju-Steven <steven@leju.com.tw>
2026-08-28 00:35:22 +0800

    更新 SerializeInterceptor 以使用動態 DTO，並在 UsersController 中應用該攔截器以支持用戶查詢

--- src/users/interceptor/serialize.interceptor.ts
+++ src/users/interceptor/serialize.interceptor.ts
@@ -6,17 +6,18 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { plainToClass } from 'class-transformer';
-import { UserDto } from '../dtos/user.dto';
+import { plainToInstance } from 'class-transformer';
 
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     // run this code before the request is handled
 
     return next.handle().pipe(
       map((data: any) => {
         // run this code after the request is handled
-        return plainToClass(UserDto, data, {
+        return plainToInstance(this.dto, data, {
           // 只包含標記了 @Expose 的屬性
           excludeExtraneousValues: true,
         });
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,6 +11,7 @@ import {
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 import { SerializeInterceptor } from './interceptor/serialize.interceptor';
 
@@ -23,7 +24,7 @@ export class UsersController {
     await this.usersService.create(body.email, body.password);
   }
 
-  @UseInterceptors(SerializeInterceptor)
+  @UseInterceptors(new SerializeInterceptor(UserDto))
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     console.log(`Finding user with id: ${id}`);
